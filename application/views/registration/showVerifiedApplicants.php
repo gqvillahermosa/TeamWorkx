71618fce72db8474efa1b941a9f7298ab5f0e2cf
Genfil Villahermosa <genfil@gmail.com>
2021-05-05 17:09:44 +0800

    complete showVerified module

---
+++ application/views/registration/showVerifiedApplicants.php
@@ -0,0 +1,29 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	
+	//print_r($verifiedApplicants);
+
+
+
+	
+?>
+
+
+<table class="table table-striped table-hover mt-5">
+		<thead><tr><th>Name</th><th>Action</th><th> </th></tr></thead>
+		<tbody>
+<?php
+		foreach ($verifiedApplicants as $key => $row)
+		{
+?>			
+	     <tr>
+	     	<td><?=anchor(base_url('registration/viewApplicant/'.$row->SeqNo), $row->Firstname.' '.$row->Surname, 'class="btn btn-info"' ) ?>  </td>
+	       <td><?=anchor(base_url('registration/acceptApplicant/'.$row->SeqNo),'Accept', 'class="btn btn-warning" 
+	       		data-target="#rejectApplicant" ') ?></td>
+	      <td><?=anchor(base_url('registration/rejectApplicant/'.$row->SeqNo),'Reject', 'class="btn btn-danger"') ?></td>
+	       </tr>
+<?php		}
+		echo '</tbody></table>';
+?>
+
+			
