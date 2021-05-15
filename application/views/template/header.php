7fd01589a98b8f9e8a5c615e99c4d81f8a0859bd
Genfil Villahermosa <genfil@gmail.com>
2021-05-15 16:32:14 +0800

    updated template/header.php created favicon from PAA logo usng gimp. two version .ico nd .png both 35x35, located at /dj

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,6 +4,8 @@
   <title>Phlippines Association of Agriculturaist, Inc.</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="type/png" href=<?=base_url("favicon.png") ?>>
+  <link rel="shortcut icon" type="image/x-icon" href=<?=base_url("favicon.ico") ?>>
   <link rel="stylesheet" href=<?=base_url('css/bootstrap.min.css') ?>>
   <script src=<?=base_url('js/jquery-3.6.0.min.js') ?>></script>
   <script src=<?=base_url('js/bootstrap.min.js') ?>></script>
