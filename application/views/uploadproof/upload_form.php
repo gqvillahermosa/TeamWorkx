6908fe033f94c02ef46281a24259df1595a39b45
Genfil Villahermosa <genfil@gmail.com>
2021-05-18 11:57:48 +0800

    delete uploadpic controller add uploadproof views

---
+++ application/views/uploadproof/upload_form.php
@@ -0,0 +1,47 @@
+<style type="text/css">
+	fieldset {
+		display: block;
+		background-color: #eeeeee;
+		margin-left: 2px;
+		margin-right: 2px;
+		padding-left: 1em;
+		padding-right: 1em;
+		border: 2px groove;	 
+		}
+	legend{
+		background-color:black;
+		color:white;
+		padding: 5px,10px 
+	}
+
+	  .form-signin {
+      width: 100%;
+      max-width:500px;
+      padding: 15px;
+      margin: auto;
+    }
+
+</style>	
+<div class="container mt-5">
+	
+	<?php echo $error;?>
+
+	<?php echo form_open_multipart(base_url('uploadproof/do_upload/id'),'class="form-signin"');?>
+	<fieldset>
+	
+			<legend>&nbsp <img src=<?=base_url("icons/upload2.svg"); ?>> Upload Proof of Payment</legend>
+			<div class="form-group">
+				<p>
+					Select the file for your proof of payment and the click the Upload button. Make sure your photo in landscape.
+				</p>
+				<input type="file" name="userfile" size="20" class="form-control mb-3" />
+				<input type="submit" value="Upload" class="btn btn-primary btn-block mb-3" />
+			</fieldset>
+			<center>
+				<small class="form-text text-muted">3 of 7 Steps</small>
+				<progress value="3" max="7"></progress>
+				</center>
+			</form>
+		</div>	
+
+</div>
